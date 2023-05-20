72f4951d2bd50c7cf3963ea08630a716d1131551
Eric Furrevig <efurrevig@gmail.com>
2023-05-20 15:39:17 -0400

    Moved notes to json server

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useEffect } from 'react'
 import NewNote from './components/NewNote'
 import Notes from './components/Notes'
 import NoteFilter from './components/NoteFilter'
+import noteService from './services/notes'
+import { setNotes } from './reducers/noteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
-  return (
-    <div>
-      <NewNote />
-      <NoteFilter />
-      <Notes  />
-    </div>
-  )
+    const dispatch = useDispatch()
+    useEffect(() => {
+        noteService
+            .getAll().then(notes => dispatch(setNotes(notes)))
+    }, [dispatch])
+
+    return (
+        <div>
+            <NewNote />
+            <NoteFilter />
+            <Notes  />
+        </div>
+    )
 }
 
 export default App
